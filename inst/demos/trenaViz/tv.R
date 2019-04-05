a87153fb275f74966787f459b77ab638f43b2330
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-04-05 09:31:12 -0700

    trenaViz demo polished up with makefile, opening of browser

--- inst/demos/trenaViz/tv.R
+++ inst/demos/trenaViz/tv.R
@@ -1,4 +1,4 @@
 library(TrenaViz)
 tv <- TrenaViz("TrenaProjectLiver")
+later(function(){browseURL("http://0.0.0.0:6838")}, 5)
 runApp(createApp(tv, port=6838))
-later(function(){browseURL("http://0.0.0.0:6838")}, 2)
