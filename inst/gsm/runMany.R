ea7cd3272314486c72bb331229ecc934a0c7f92d
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-04-12 17:41:28 -0700

    4 gene gsm test works both lapply and bplapply

--- inst/gsm/runMany.R
+++ inst/gsm/runMany.R
@@ -15,35 +15,26 @@ library(BatchJobs)
 stopifnot(packageVersion("trena") >= "1.5.13")
 stopifnot(packageVersion("trenaSGM") >= "0.99.76")
 #------------------------------------------------------------------------------
-configurationFile <- "configUtils.R"
+configurationFile <- "config.R"
 stopifnot(file.exists(configurationFile))
 
 if(!exists("configurationFileRead") || !configurationFileRead)
   source(configurationFile)
 #------------------------------------------------------------------------------
-if(interactive()){
-   goi <- test.goi
-   startGeneIndex <- 1
-   endGeneIndex <- 2
-   }
-
 if(!interactive()){
    args <- commandArgs(trailingOnly=TRUE)
    stopifnot(length(args) == 2)
    startGeneIndex <- as.integer(args[1])
    endGeneIndex <- as.integper(args[2])
+   printf("running with genes %d - %d", startGeneIndex, endGeneIndex)
    }
-
-printf("running with genes %d - %d", startGeneIndex, endGeneIndex)
 #-----------------------------------------------------------------------------
 stopifnot(exists("trenaProject"))
 stopifnot(exists("mtx"))
 stopifnot(exists("goi"))
 
 if(!file.exists(OUTPUTDIR)) dir.create(OUTPUTDIR)
-if(!file.exists(fp.logDir)) dir.create(fp.logDir)
-if(!file.exists(tfMapping.logDir)) dir.create(tfMapping.logDir)
-if(!file.exists(model.logDir)) dir.create(model.logDir)
+if(!file.exists(LOGDIR)) dir.create(LOGDIR)
 #----------------------------------------------------------------------------------------------------
 basic.build.spec <- list(title="footprint-based-tf-model-builder-for-GTEx-liver",
                          type="footprint.database",
@@ -65,16 +56,23 @@ basic.build.spec <- list(title="footprint-based-tf-model-builder-for-GTEx-liver"
 #----------------------------------------------------------------------------------------------------
 buildModel <- function(short.spec)
 {
-
-   required.fields <- c("targetGene")
+   required.fields <- c("targetGene", "runParallel")
    missing.fields <- setdiff(required.fields, names(short.spec))
+
    if(length(missing.fields) > 0){
-      msg <- sprintf("runStagedSGM.footprints finds fields missing in short.spec: %s", paste(missing.fields, collapse=", "))
+      msg <- sprintf("buildModel finds fields missing in short.spec: %s", paste(missing.fields, collapse=", "))
       stop(msg)
       }
 
+   printf("********* [building model for %s, parallel = %s]", short.spec$targetGene, short.spec$runParallel)
+
+
    spec <- basic.build.spec
    targetGene <- short.spec$targetGene
+
+   filename <- sprintf("%s/%s.RData", OUTPUTDIR, targetGene)
+   system(sprintf("touch %s", filename))  # so an empty file exists if the model building fails
+
    tbl.geneLoc <- subset(tbl.geneInfo, geneSymbol==targetGene)[1,]
    chromosome <- tbl.geneLoc$chrom
    tss <- tbl.geneLoc$tss
@@ -84,55 +82,59 @@ buildModel <- function(short.spec)
    spec$targetGene <- targetGene
    spec$tss=tss
    spec$regions <- determineRegulatoryRegions(targetGene)
-   xyz <- "regions specifiedl"
 
    spec$geneSymbol <- targetGene
 
    builder <- FootprintDatabaseModelBuilder(genomeName, targetGene, spec, quiet=FALSE)
-   x <- build(builder)
-   return(x)
+   results <- build(builder)
+
+   save(results, file=filename)
+
+   return(results)
 
 } # buildModel
 #----------------------------------------------------------------------------------------------------
-do.run <- function(genes)
+do.run <- function(genes, parallel=TRUE)
 {
    short.specs <- lapply(genes, function(gene)
                                     list(targetGene=gene,
-                                         regionsMode="enhancers"))
+                                         regionsMode="enhancers",
+                                         runParallel=parallel))
 
    names(short.specs) <- as.character(genes)
 
-   # x <- lapply(short.specs, buildModel)
-   # names(x) <- genes
-   # xyz <- "all done"
+   if(parallel){
+      bp.params <- MulticoreParam(stop.on.error=FALSE, log=TRUE, logdir=LOGDIR, threshold="INFO", workers=WORKERS)
+      printf("running bplapply on %d genes", length(genes))
+      results <- bptry({bplapply(short.specs, buildModel, BPPARAM=bp.params)})
+    } else {
+      results <- lapply(short.specs, buildModel)
+      names(results) <- genes
+      }
 
-   bp.params <- MulticoreParam(stop.on.error=FALSE, log=TRUE, logdir=fp.logDir, threshold="INFO", workers=WORKERS)
-   results.fp <<- bptry({bplapply(short.specs, buildModel, BPPARAM=bp.params)})
+   invisible(results)
 
 } # do.run
+#----------------------------------------------------------------------------------------------------
+demoGenes <- function()
+{
+   genes <- c("GSTM1", "RBMXP2", "INKA2", "APOE")
+
+   return(genes)
+
+} # demoGenes
 #----------------------------------------------------------------------------------------------------
 # GSTM1:  a "normal" gene, protein-coding, variable expression, with geneHancer regions
 # RBMXP2: as above, but a pseuodgene with no footprints
 # INKA2:  as above, but with no geneHancer regions
-test_threeGenes <- function()
+# APOE: well-behaved should produce a strong model
+test_fourGenes <- function(useParallel=FALSE)
 {
-   genes <- c("GSTM1", "RBMXP@", "INKA2")
-   genes <- c("GSTM1", "INKA2")
+   genes <- demoGenes()
+   x <- do.run(genes, parallel=useParallel)
+   checkEquals(length(x), 4)
+   length(x)
+   x
 
-   do.run(genes)
-
-} # test_threeGenes
-#----------------------------------------------------------------------------------------------------
-debug.RBMXP2 <- function()
-{
-   genes <- c("GSTM1", "RBMXP2")
-   short.specs <- lapply(genes, function(gene)
-                                    list(targetGene=gene,
-                                         regionsMode="enhancers"))
-
-   names(short.specs) <- as.character(genes)
-
-   xyz <- buildModel(short.spec[[1]])
-
-} # debug.RBMXP2
+} # test_fourGenes
 #----------------------------------------------------------------------------------------------------
